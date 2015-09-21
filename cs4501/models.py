e86b61a13985e3f5c31da69ab5a10b23370aa85c
Sumeet Babu <babu.sumeet1@gmail.com>
2015-09-21 17:30:46 +0000

    Typos

--- cs4501/models.py
+++ cs4501/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class User(models.Model):
 	Artist = 'artist'
@@ -16,21 +17,19 @@ class User(models.Model):
 	last_name = models.CharField(max_length=16)
 	password = models.CharField(max_length=16)
 	is_active = models.BooleanField()
-	type_of_user = modelsCharField(max_length=16, choices=Type_Of_User_list, default=General)
-	listings = models.OneToManyField(Listing)
+	type_of_user = models.CharField(max_length=16, choices=Type_Of_user_List, default=General)
 	type_of_instrument = models.CharField(max_length=16, null=True, blank=True)
-	reviews = models.OneToManyField(Review)
 
 class Listing(models.Model):
-	title = models.Charfield(max_length=16)
+	title = models.CharField(max_length=16)
 	description = models.TextField(blank=True)
-	creator = models.ForeinKey('User')
+	creator = models.ForeignKey(User)
 	date_listed = models.DateTimeField()
 	available = models.BooleanField()
 
 class Review(models.Model):
-	title = models.CharField(max_length = 40))
-	body = models.CharField(max_length = 250))
+	title = models.CharField(max_length=40)
+	body = models.CharField(max_length=250)
 	review_rating = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(5)])
 	reviewer = models.ForeignKey(User)
 	
